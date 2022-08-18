14e026e55f59ec25268785e5549444e261fb3d36
RONIT SINGH <ronitsingh7003@gmail.com>
2022-08-18 11:54:21 +0530

    #6 Personal Navigator

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('''Hello Ronit''')
+    return HttpResponse('''<h1>Hello Ronit</h1> <p> Go to Google : <a href="https://www.google.com/">Google</a></p>''')
 
 def about(request):
     return HttpResponse("About Ronit")
