b69e828689bb39308be6da26ac94fa19503f34ec
RONIT SINGH <ronitsingh7003@gmail.com>
2022-08-18 14:17:09 +0530

    #10 Coding The Backend of Remove Punctuation

--- textutils/textutils/urls.py
+++ textutils/textutils/urls.py
@@ -3,12 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('removepunc', views.removepunc, name='rempun'),
-    path('capitalizefirst', views.capfirst, name='capfirst'),
-    path('newlineremove', views.newlineremove, name='newlineremove'),
-    path('spaceremove', views.spaceremove, name='spaceremove'),
-    path('charcount', views.charcount, name='charcount'),
-
+   path('admin/', admin.site.urls),
+   path('', views.index, name='index'),
+   path('analyze', views.analyze, name='analyze')
 ]
